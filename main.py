3d8e60d83a0ed3dae0a7872022db6327705ae37c
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-01-12 16:37:19 -0600

    Create a sorted report of each letter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from string import ascii_lowercase 
+
 def get_word_count(book_text):
     return len(book_text.split())
 
@@ -10,10 +12,27 @@ def get_char_count(book_text):
             chars[c] += 1
     return chars
 
+def sort_on_value(t):
+    return t[1] # sort on value of tuple
+
+def get_book_stats(book_name, book_text):
+    word_count = get_word_count(book_text)
+    chars = get_char_count(book_text)
+    header = f"--- Begin report of {book_name} ---\n"
+    footer = f"--- End report ---\n"
+    body = f"{word_count} words found in the document\n\n"
+    vals = []
+    for c in ascii_lowercase:
+        if c in chars:
+           vals.append((c, chars[c]))
+    vals.sort(key=sort_on_value, reverse=True)
+    for n, l in vals:
+        body += f"The '{l} character was found {n} times\n"
+    return header + body + footer
+
 if __name__ == '__main__':
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
-    print(get_word_count(file_contents))
-    print(get_char_count(file_contents))
+    print(get_book_stats(path_to_file, file_contents))
 
