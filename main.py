f44ac86ea288127b21be7656847540fec731c470
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-01-12 15:28:53 -0600

    Count words

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-path_to_file = "books/frankenstein.txt"
-with open(path_to_file) as f:
-    file_contents = f.read()
-print(file_contents)
+def get_word_count(book_text):
+    return len(book_text.split())
+
+if __name__ == '__main__':
+    path_to_file = "books/frankenstein.txt"
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    print(get_word_count(file_contents))
 
