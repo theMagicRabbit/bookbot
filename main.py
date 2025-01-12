903194283709142ef1faff033ad0e3112c586a9c
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-01-12 15:45:23 -0600

    Count each character in book

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 def get_word_count(book_text):
     return len(book_text.split())
 
+def get_char_count(book_text):
+    chars = {}
+    for c in book_text.lower():
+        if c not in chars:
+            chars[c] = 1
+        else:
+            chars[c] += 1
+    return chars
+
 if __name__ == '__main__':
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
     print(get_word_count(file_contents))
+    print(get_char_count(file_contents))
 
